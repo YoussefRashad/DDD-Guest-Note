2315b0a62712c1e0e0ea47fd034bd1fcb674db4f
Youssef Rashad <40126657+YoussefRashad@users.noreply.github.com>
2022-01-31 16:20:37 +0200

    add file model

---
+++ app/Models/File.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm';
+import Note from './Note';
+
+export default class File extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public media_url: string
+
+  @column()
+  public note_id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  // relationships
+  @belongsTo(() => Note)
+  public note: BelongsTo<typeof Note>
+}
--- app/Models/Note.ts
+++ app/Models/Note.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm';
 import User from './User';
 import NoteType from './NoteType';
 
@@ -14,13 +14,13 @@ export default class Note extends BaseModel {
   public message: string
 
   @column()
+  public file_id: string
+
+  @column()
   public note_type_id: number
 
   @column()
   public user_id: number
-
-  @column()
-  public files: string[]
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
@@ -34,4 +34,7 @@ export default class Note extends BaseModel {
 
   @belongsTo(() => NoteType)
   public note_type: BelongsTo<typeof NoteType>
+
+  @hasMany(() => Note, { foreignKey: 'note_id' })
+  public files: HasMany<typeof Note>
 }
